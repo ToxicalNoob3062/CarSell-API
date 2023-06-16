bec60cf5a2327a910bf2faaf19f1165ba6ff47d8
toxical-noob <rahat3062@gmail.com>
2023-06-16 22:46:47 +0600

    Added findUser with ID , with Email , Delete User & Patch User to differrent API routes!

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user-dto';
 import { UsersService } from './users.service';
 
 
@@ -13,4 +14,22 @@ export class UsersController {
     createUser(@Body() body: CreateUserDto) {
         this.usersService.create(body.email, body.password);
     }
-}
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email);
+    }
+    @Delete('/:id')
+    deleteUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id));
+    }
+    @Patch("/:id")
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id), body);
+    }
+};
