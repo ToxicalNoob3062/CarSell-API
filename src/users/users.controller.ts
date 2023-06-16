fa68dc71d30a7f919ddb6d18d7e32f9fc420a146
toxical-noob <rahat3062@gmail.com>
2023-06-16 23:21:27 +0600

    Added utility method to throw http404 error and converted GET , DELETE & PATH method to async for using that utility method after retrieving user from another async!

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user-dto';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 
 @Controller('/auth')
@@ -10,26 +11,36 @@ export class UsersController {
         private usersService: UsersService,
     ) { }
 
+    //controller utility modified method
+    http404(user: User, id: string) {
+        return user ? user : (() => { throw new NotFoundException(`Could not find an user with an id of ${id}!`); })();
+    }
+
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         this.usersService.create(body.email, body.password);
     }
 
     @Get('/:id')
-    findUser(@Param('id') id: string) {
-        return this.usersService.findOne(parseInt(id));
+    async findUser(@Param('id') id: string) {
+        const user = await this.usersService.findOne(parseInt(id));
+        return this.http404(user, id);
     }
 
     @Get()
     findAllUsers(@Query('email') email: string) {
         return this.usersService.find(email);
     }
+
     @Delete('/:id')
-    deleteUser(@Param('id') id: string) {
-        return this.usersService.remove(parseInt(id));
+    async deleteUser(@Param('id') id: string) {
+        const user = await this.usersService.remove(parseInt(id));
+        return this.http404(user, id);
     }
+
     @Patch("/:id")
-    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-        return this.usersService.update(parseInt(id), body);
+    async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        const user = await this.usersService.update(parseInt(id), body);
+        return this.http404(user, id);
     }
 };
