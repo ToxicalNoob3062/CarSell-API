eab6fd3444bf335a0781b33990f7cb5f0b2c8812
toxical-noob <rahat3062@gmail.com>
2023-06-16 22:40:38 +0600

    Added Update User DTO for validating the body of PATCH method inside users controller!

---
+++ src/users/dto/update-user-dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsString, IsOptional } from "class-validator";
+
+
+export class UpdateUserDto {
+    @IsEmail()
+    @IsOptional()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    password: string;
+}
