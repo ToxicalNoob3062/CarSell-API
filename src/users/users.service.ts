0eab4f4554695baae3c2357384c584cf21c48498
toxical-noob <rahat3062@gmail.com>
2023-06-16 22:48:33 +0600

    Implemented corrospondent service methods to sync with the controller routes that were newly created in latest commit!

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 
 @Injectable()
@@ -13,5 +13,21 @@ export class UsersService {
     create(email: string, password: string) {
         const userInstance = this.repo.create({ email, password });
         return this.repo.save(userInstance);
+    }
+    findOne(id: number) {
+        return this.repo.findOneBy({ id });
+    }
+    find(email: string) {
+        return this.repo.find({ where: { email } });
+    }
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+        if (!user) throw new Error(`Could not find an user with an id of ${id}!`);
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        return user ? this.repo.remove(user) : (() => { throw new Error(`Could not find an user with an id of ${id}!`); })();
     }
 }
