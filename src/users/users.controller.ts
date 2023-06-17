ecf2762c6adb83d5c70154a97d31d73afd267913
toxical-noob <rahat3062@gmail.com>
2023-06-17 11:14:50 +0600

    Instead of passing serialize interceptor directly called it as an instance which recevies dto class as its private field!

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import { UpdateUserDto } from './dto/update-user-dto';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from 'src/users/dto/user.dto';
 
 
 @Controller('/auth')
@@ -18,34 +19,34 @@ export class UsersController {
     //controller utility modified method
     http404(user: User, id: string) {
         return user ? user : (() => { throw new NotFoundException(`Could not find an user with an id of ${id}!`); })();
-    }
+    };
 
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         this.usersService.create(body.email, body.password);
-    }
+    };
 
-    @UseInterceptors(SerializeInterceptor) //pushed our custom interceptor for filtering props on response.
+    @UseInterceptors(new SerializeInterceptor(UserDto)) //pushed our custom interceptor for filtering props on response.
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.usersService.findOne(parseInt(id));
         return this.http404(user, id);
-    }
+    };
 
     @Get()
     findAllUsers(@Query('email') email: string) {
         return this.usersService.find(email);
-    }
+    };
 
     @Delete('/:id')
     async deleteUser(@Param('id') id: string) {
         const user = await this.usersService.remove(parseInt(id));
         return this.http404(user, id);
-    }
+    };
 
     @Patch("/:id")
     async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
         const user = await this.usersService.update(parseInt(id), body);
         return this.http404(user, id);
-    }
+    };
 };
