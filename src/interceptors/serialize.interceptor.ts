a076111911e47843dbbcee648886c6d77486b7c7
toxical-noob <rahat3062@gmail.com>
2023-06-17 11:13:03 +0600

    Made the serialize interceptor reusable by pugging in desired dto class i want by the help of the constructor function!

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -2,12 +2,13 @@ import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from "rxjs/operators";
 import { plainToClass } from 'class-transformer';
-import { UserDto } from 'src/users/dto/user.dto';
+import { Class } from 'src/custom.types';
 
 export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: Class) { }
     intercept(_: ExecutionContext, next: CallHandler): Observable<any> {
         return next.handle().pipe(map((data => {
-            return plainToClass(UserDto, data, {
+            return plainToClass(this.dto, data, {
                 excludeExtraneousValues: true, //Tells to export prop with @Expose decorators only.
             });
         })));
