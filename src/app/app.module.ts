ece2fad8c4c98cfedd8162369c7688ca0e0df253
toxical-noob <rahat3062@gmail.com>
2023-06-19 09:06:01 +0600

    Wrote an authentication system e2e test and for that I have to move my middleware and global validation pipe inside App module for test supporting and following the Nest recommended way.Also installed a config packgae to support multiple db based on prod and dev environment.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from '../users/users.module';
@@ -7,10 +7,12 @@ import { ReportsModule } from '../reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { Report } from '../reports/report.entity';
+import { APP_PIPE } from '@nestjs/core';
+import cookieSession = require('cookie-session'); //Because nestJS dont support es-version for this module.
 
 const OrmModule = TypeOrmModule.forRoot({
   type: "sqlite",
-  database: "db.sqlite",
+  database: `db.sqlite`,
   entities: [User, Report],
   synchronize: true,
 });
@@ -18,6 +20,17 @@ const OrmModule = TypeOrmModule.forRoot({
 @Module({
   imports: [UsersModule, ReportsModule, OrmModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    //This is how you set global pipes inside App module instead of bootstrap function!
+    provide: APP_PIPE,
+    useValue: new ValidationPipe({ whitelist: true })
+  }],
 })
-export class AppModule { };
+export class AppModule {
+  //setting up middle ware globally inside app module instead of bootstrap function
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({
+      keys: ["ola123"]
+    })).forRoutes('*');
+  };
+};
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
-import cookieSession = require("cookie-session"); //Because nestJS dont support es-version for this module.
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ["ola123"]
-  }));
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true })
-  );
   await app.listen(3000);
 };
 bootstrap();
