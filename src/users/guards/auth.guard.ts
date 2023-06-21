83d1920ccc82573a21b50a2c1da8174aad5b0ce0
toxical-noob <rahat3062@gmail.com>
2023-06-21 23:38:29 +0600

    bug fix for auth guard.

--- src/users/guards/auth.guard.ts
+++ src/users/guards/auth.guard.ts
@@ -3,6 +3,6 @@ import { ExecutionContext, CanActivate } from '@nestjs/common';
 export class AuthGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
         const req = context.switchToHttp().getRequest();
-        return req.session.userId;
+        return req.session?.userId;
     }
 }
